4ccbb4b2ab52a00dc209715521772582fea40737
Byran Wills-Heath <byran@adgico.co.uk>
2016-07-01 00:03:35 +0100

    Added Access-Control-Allow-Origin to http response

--- raspberry_pi/traffic_light_server.py
+++ raspberry_pi/traffic_light_server.py
@@ -22,5 +22,12 @@ def green():
     print("green")
     return 'OK'
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
